a1dba5e8f389ebce35d4ed3dd3a72c2e74f95fe2
SnehalKawade <snehkawade@gmail.com>
2023-09-20 21:39:55 +0530

    Add project files.

---
+++ Model_AuthenticationDemo/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Model_AuthenticationDemo.Controllers
+{
+    [Authorize(Roles = "Admin")]
+
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+        UserManager<IdentityUser> UserManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> UserManager)
+        {
+            this.roleManager = roleManager;
+            this.UserManager = UserManager;
+
+        }
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+
+        }
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Model_AuthenticationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Model_AuthenticationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
